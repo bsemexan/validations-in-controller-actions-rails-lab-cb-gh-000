1108374baee5dddc7b675d9b758087512d03a1ce
Berry  Semexan <bsemexan@gmail.com>
2018-03-27 20:03:32 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presense: true
-  validates :content, minimum: 100
+  validates :content, length: { minimum: 100 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  
+
 end
